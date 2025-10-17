7eb88fdf556f827a2d1a3b1ccae02b9d622d8361
ddeuterio <daniel.garcia.anes@gmail.com>
2025-10-17 15:55:16 +0200

    add: db image vuln schema

--- docker/db/init.sql
+++ docker/db/init.sql
@@ -25,4 +25,31 @@ CREATE TABLE nvd_sync (
     id TEXT PRIMARY KEY,
     last_fetched TEXT NOT NULL,
     chcksum TEXT NOT NULL
-)
+);
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT
+);
+
+CREATE TABLE images (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    version TEXT NOT NULL,
+    product TEXT REFERENCES products(name) ON DELETE CASCADE
+);
+
+CREATE TABLE image_vulnerabilities (
+    image_name TEXT NOT NULL REFERENCES images(name) ON DELETE CASCADE,
+    image_version TEXT NOT NULL REFERENCES images(version) ON DELETE CASCADE,
+    cve TEXT NOT NULL REFERENCES vulnerabilities(cve_id) ON DELETE CASCADE,
+    fix_versions TEXT,
+    first_seen TIMESTAMPTZ,
+    last_seen TIMESTAMPTZ,
+    affected_component_type TEXT NOT NULL,
+    affected_component TEXT NOT NULL,
+    affected_version TEXT NOT NULL,
+    affected_path TEXT NOT NULL,
+    PRIMARY KEY (image_name, image_version, cve)
+);
