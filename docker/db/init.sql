2ff1f88466d0f5257e57e02170b5875461a59b04
ddeuterio <daniel.garcia.anes@gmail.com>
2025-11-01 19:08:05 +0100

    add: docker changes to support vma app

--- docker/db/init.sql
+++ docker/db/init.sql
@@ -28,22 +28,22 @@ CREATE TABLE nvd_sync (
 );
 
 CREATE TABLE products (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
+    id TEXT NOT NULL PRIMARY KEY,
     description TEXT
 );
 
 CREATE TABLE images (
-    id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     version TEXT NOT NULL,
-    product TEXT REFERENCES products(name) ON DELETE CASCADE
+    product TEXT NOT NULL REFERENCES products(id) ON DELETE CASCADE,
+    PRIMARY KEY (name, version, product)
 );
 
 CREATE TABLE image_vulnerabilities (
-    image_name TEXT NOT NULL REFERENCES images(name) ON DELETE CASCADE,
-    image_version TEXT NOT NULL REFERENCES images(version) ON DELETE CASCADE,
-    cve TEXT NOT NULL REFERENCES vulnerabilities(cve_id) ON DELETE CASCADE,
+    image_name TEXT NOT NULL,
+    image_version TEXT NOT NULL,
+    product TEXT NOT NULL,
+    cve TEXT NOT NULL REFERENCES vulnerabilities(cve_id),
     fix_versions TEXT,
     first_seen TIMESTAMPTZ,
     last_seen TIMESTAMPTZ,
@@ -51,5 +51,6 @@ CREATE TABLE image_vulnerabilities (
     affected_component TEXT NOT NULL,
     affected_version TEXT NOT NULL,
     affected_path TEXT NOT NULL,
-    PRIMARY KEY (image_name, image_version, cve)
+    PRIMARY KEY (image_name, image_version, product, cve, affected_component_type, affected_component),
+    FOREIGN KEY (image_name, image_version, product) REFERENCES images(name, version, product) ON DELETE CASCADE
 );
